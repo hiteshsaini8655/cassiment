6c8ed49a13a1f83df5b61930d724b018b847321c
regex software <sainihitesh2512@gmail.com>
2023-08-22 10:29:11 +0530

    todys work

---
+++ 220823.cpp
@@ -0,0 +1,72 @@
+//area of //gm using base 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   float height,base,area;
+   cout<<"enter the value for base and height\n";
+   cin>>base>>height;
+   area=base*height;
+   cout<<"area of"<<area;
+    
+
+    return 0;
+}
+=====================================================================
+//area of //gm using base 
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+using namespace std;
+int main()
+{
+   const float pi =3.14159;
+   float radius,sa;
+   cout<<"enter the value for radius and surface";
+   cin>>radius;
+   sa=4*pi*pow(radius,2);
+   cout<<"surface area is: "<<sa<<"\n";
+   sa=33;
+   cout<<sa<<"\n";
+   
+   
+   
+    
+
+    return 0;
+}
+=========================================================================
+//area of rohmbus using its diagonal
+//area //diagonal
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   float d1,d2,area;
+   cout<<"enter value for d1 and d2\n";
+   cin>>d1>>d2;
+   area=(d1*d2)/2;
+   cout<<"area ot rohmbus is : "<<area;
+    return 0;
+}
+==================================================================
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a,b,sum;
+    cout<<"enter value for a and b\n";
+    cin>>a>>b;
+    sum =a+b;
+    if(sum>18)
+      cout<<"sum is greater than 18";
+      else
+      cout<<"sum less then 18";
+      
+    return 0;
+}
+======================================================
